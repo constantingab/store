4f38777e6c06ee88b558a33efa15ec4280f09600
Konstantin Gabelok <constantingab@gmail.com>
2024-09-23 20:17:18 +0300

    add api routing

---
+++ backend/routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\API\CategoryController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/categories', [CategoryController::class, 'index']);
