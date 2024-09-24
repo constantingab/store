6b0453f66d57fa23753652f0b9208917f0f1bcd9
Konstantin Gabelok <constantingab@gmail.com>
2024-09-24 12:07:03 +0300

    save before Product model create

--- backend/app/Http/Controllers/API/CategoryController.php
+++ backend/app/Http/Controllers/API/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -12,7 +13,9 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return "all categories";
+
+        $categories = Category::with('products')->get();
+        return response()->json($categories);
     }
 
     /**
